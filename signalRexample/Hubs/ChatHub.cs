eb152e01cdc6550fce3a612dc12132c50367387c
tarekdh <tarek.khachnawi@gmail.com>
2020-03-11 23:03:25 +0100

    first commit

---
+++ signalRexample/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace signalRexample.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public Task SendMessage(string user, string message)
+        {
+            var msg = $"{user} says you {message}";
+            return Clients.Others.SendAsync("ReceiveMessage", msg);
+        }
+    }
+}
